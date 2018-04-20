27f0341c3f974154785ed8f3a6c41d14d0044a34
Michail Urakov <mikbox74@gmail.com>
2018-04-20 20:14:23 +0300

    Hotfix for Breadcrumbs widget

--- src/Widgets/Breadcrumbs.php
+++ src/Widgets/Breadcrumbs.php
@@ -28,28 +28,95 @@ namespace mikbox74\Chaldene\Widgets;
 use Yii;
 use yii\helpers\Html;
 use mikbox74\Chaldene\Base\ChaldeneWidget;
+use yii\base\InvalidConfigException;
+use yii\helpers\ArrayHelper;
 
 /**
- * Description of Breadcrumbs
+ * This is a clone of \yii\widgets\Breadcrumbs with some little improvements
  *
  * @author Михаил Ураков <mikbox74@gmail.com>
+ * @author Qiang Xue <qiang.xue@gmail.com>
  */
 class Breadcrumbs extends ChaldeneWidget
 {
-
+    /**
+     * @var string the name of the breadcrumb container tag.
+     */
     public $tag = 'span';
 
+    /**
+     * @var array the HTML attributes for the breadcrumb container tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
     public $itemTemplate = "{link}\n";
 
+    /**
+     * @var bool whether to HTML-encode the link labels.
+     */
     public $activeItemTemplate = "<span class=\"active\">{link}</span>\n";
 
+    /**
+     * @var array the HTML attributes for the breadcrumb container tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
     public $options = [];
 
+    /**
+     * @var string HTML or plain text between items.
+     */
     public $separator = '/';
 
+    /**
+     * @var array list of links to appear in the breadcrumbs. If this property is empty,
+     * the widget will not render anything. Each array element represents a single link in the breadcrumbs
+     * with the following structure:
+     *
+     * ```php
+     * [
+     *     'label' => 'label of the link',  // required
+     *     'url' => 'url of the link',      // optional, will be processed by Url::to()
+     *     'template' => 'own template of the item', // optional, if not set $this->itemTemplate will be used
+     * ]
+     * ```
+     *
+     * If a link is active, you only need to specify its "label", and instead of writing `['label' => $label]`,
+     * you may simply use `$label`.
+     *
+     * Since version 2.0.1, any additional array elements for each link will be treated as the HTML attributes
+     * for the hyperlink tag. For example, the following link specification will generate a hyperlink
+     * with CSS class `external`:
+     *
+     * ```php
+     * [
+     *     'label' => 'demo',
+     *     'url' => 'http://example.com',
+     *     'class' => 'external',
+     * ]
+     * ```
+     *
+     * Since version 2.0.3 each individual link can override global [[encodeLabels]] param like the following:
+     *
+     * ```php
+     * [
+     *     'label' => '<strong>Hello!</strong>',
+     *     'encode' => false,
+     * ]
+     * ```
+     */
     public $links = [];
 
+    /**
+     * @var array the first hyperlink in the breadcrumbs (called home link).
+     * Please refer to [[links]] on the format of the link.
+     * If this property is not set, it will default to a link pointing to [[\yii\web\Application::homeUrl]]
+     * with the label 'Home'. If this property is false, the home link will not be rendered.
+     */
     public $homeLink;
+
+    /**
+     * @var bool whether to HTML-encode the link labels.
+     */
+    public $encodeLabels = true;
 
     public function init()
     {
@@ -57,6 +124,7 @@ class Breadcrumbs extends ChaldeneWidget
         $params = $this->getView()->params;
         $this->links = !empty($params['breadcrumbs']) ? $params['breadcrumbs'] : [];
     }
+
     /**
      * Renders the widget.
      */
@@ -82,5 +150,34 @@ class Breadcrumbs extends ChaldeneWidget
                     ? $this->itemTemplate : $this->activeItemTemplate);
         }
         echo Html::tag($this->tag, implode(' ' . $this->separator . ' ', $links), $this->options);
+    }
+
+    /**
+     * Renders a single breadcrumb item.
+     * @param array $link the link to be rendered. It must contain the "label" element. The "url" element is optional.
+     * @param string $template the template to be used to rendered the link. The token "{link}" will be replaced by the link.
+     * @return string the rendering result
+     * @throws InvalidConfigException if `$link` does not have "label" element.
+     */
+    protected function renderItem($link, $template)
+    {
+        $encodeLabel = ArrayHelper::remove($link, 'encode', $this->encodeLabels);
+        if (array_key_exists('label', $link)) {
+            $label = $encodeLabel ? Html::encode($link['label']) : $link['label'];
+        } else {
+            throw new InvalidConfigException('The "label" element is required for each link.');
+        }
+        if (isset($link['template'])) {
+            $template = $link['template'];
+        }
+        if (isset($link['url'])) {
+            $options = $link;
+            unset($options['template'], $options['label'], $options['url']);
+            $link = Html::a($label, $link['url'], $options);
+        } else {
+            $link = $label;
+        }
+
+        return strtr($template, ['{link}' => $link]);
     }
 }
